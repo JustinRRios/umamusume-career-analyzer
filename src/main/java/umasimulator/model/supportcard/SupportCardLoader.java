e6ccfa2635fc4cc8e1596224534f366aa6c165e8
Justin Rios <justin.ray.rios@gmail.com>
2026-07-31 05:54:24 -0700

    chore: removed unused import

--- src/main/java/umasimulator/model/supportcard/SupportCardLoader.java
+++ src/main/java/umasimulator/model/supportcard/SupportCardLoader.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.EnumMap;
 
 public class SupportCardLoader {
     private static final ObjectMapper objectMapper = new ObjectMapper();
